86cb37da0c7ee8db11fdcdff9b3352bd0e4acf1e
shubham patil <patil.shubham3@mahfin.com>
2022-11-29 09:15:05 +0530

    Changes

--- src/routes/dcgMaster.js
+++ src/routes/dcgMaster.js
@@ -3,23 +3,27 @@ const excelController = require("../services/SchemeDcg");
 const upload = require("../middlewares/upload");
 // const { BadRequestError, NotFoundError, UnauthorizedError } = require('../core/Error');
 // const { errorResponse } = require('../core/Response');
-
+const rateLimiterMiddleware = require('../services/RateLimiter');
+const logger = require('../core/Logger');
+const { UnauthorizedError } = require('../core/Error');
+const { errorResponse } = require('../core/Response');
+const { ValidateToken } = require('../services/Token');
 const router = express.Router();
-// router.use(rateLimiterMiddleware);
+router.use(rateLimiterMiddleware);
 
-// router.use(async (req, res, next) => {
-//     try{
-//         logger.info('Time: '+ Date.now());
-//         const isValid = await ValidateToken(req);
-//         if(isValid){
-//             next()
-//         }else{
-//             throw UnauthorizedError("Invalid Token")
-//         }
-//     }catch(err){
-//         errorResponse(err,res);
-//     }
-// });
+router.use(async (req, res, next) => {
+    try{
+        logger.info('Time: '+ Date.now());
+        const isValid = await ValidateToken(req);
+        if(isValid){
+            next()
+        }else{
+            throw UnauthorizedError("Invalid Token")
+        }
+    }catch(err){
+        errorResponse(err,res);
+    }
+});
 
 router.post("/uploaddcg", upload.single("file"), excelController.upload);
 
--- src/routes/pcgMaster.js
+++ src/routes/pcgMaster.js
@@ -2,7 +2,27 @@ const express = require("express");
 const excelController = require("../services/SchemePcg");
 const upload = require("../middlewares/upload");
 const router = express.Router();
+const rateLimiterMiddleware = require('../services/RateLimiter');
+const logger = require('../core/Logger');
+const { UnauthorizedError } = require('../core/Error');
+const { errorResponse } = require('../core/Response');
+const { ValidateToken } = require('../services/Token');
 
+router.use(rateLimiterMiddleware);
+
+router.use(async (req, res, next) => {
+    try{
+        logger.info('Time: '+ Date.now());
+        const isValid = await ValidateToken(req);
+        if(isValid){
+            next()
+        }else{
+            throw UnauthorizedError("Invalid Token")
+        }
+    }catch(err){
+        errorResponse(err,res);
+    }
+});
 router.post("/uploadpcg", upload.single("file"), excelController.upload);
 
 module.exports = router;
